7b0aa097445c964c0cf8065f27462d0bd417b9aa
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2022-10-07 21:32:05 +0700

    uppdate

--- Exercise-3/HangmanService.cs
+++ Exercise-3/HangmanService.cs
@@ -8,22 +8,48 @@ namespace Exercise_3
 {
     internal class HangmanService
     {
-        string[] vocabulary = { "Softsquaregroup", "Tarveller" };
+        //string[] vocabulary = { "sheep", "goat", "computer", "america", "watermelon", "icecream", "jasmine", "pineapple", "orange", "mango" };
+        string[] vocabulary = { "goat" };
+        string mysteryWord;
+        char[] guess;
+        int remaining;
+        enum check { Correct, Incorrect, Duplicate }
+
         public void Restart()
         {
-
+            Console.WriteLine("Welcome to Hangman game");
+            Random randGen = new Random();
+            var idx = randGen.Next(0, vocabulary.Length - 1);
+            mysteryWord = vocabulary[idx];
+            Console.WriteLine(mysteryWord);
+            Console.WriteLine();
         }
         public void GetDisplay()
         {
-
+            guess = new char[mysteryWord.Length];
+            for (int p = 0; p < mysteryWord.Length; p++)
+            {
+                guess[p] = '*';
+            }
         }
-        public void Input()
+        public void Input(char playerGuess)
         {
-
+            for (int j = 0; j < mysteryWord.Length; j++)
+            {
+                if (playerGuess == mysteryWord[j])
+                {
+                    guess[j] = playerGuess;
+                }
+                Console.Write(guess[j]);
+            }
+            Console.WriteLine("\nguess.Length: " + guess.Length);
+            Console.WriteLine("mysteryWord.Length: " + mysteryWord.Length);
+            Console.WriteLine();
+            //return Enum.GetValues(typeof(check));
         }
         public int GetRemainingTry()
         {
-            return 0;
+            return remaining;
         }
     }
 }
--- Exercise-3/Program.cs
+++ Exercise-3/Program.cs
@@ -11,6 +11,26 @@ namespace Exercise_3
         static void Main(string[] args)
         {
             HangmanService hangman = new HangmanService();
+            hangman.Restart();
+
+            hangman.GetDisplay();
+
+            //Console.Write("\nplease enter character: ");
+
+            //char playerGuess = char.Parse(Console.ReadLine());
+
+            hangman.Input(char.Parse("o"));
+            hangman.Input(char.Parse("t"));
+            hangman.Input(char.Parse("g"));
+            hangman.Input(char.Parse("a"));
+            //hangman.GetRemainingTry();
+
+
+            /*while (true)
+            {
+                hangman.GetDisplay();
+                hangman.Input();
+            }*/
         }
     }
 }
