d6d1ad586378642ee5d472af702871ffa84a74a0
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2022-10-07 13:33:19 +0700

    Add project files.

---
+++ Exercise-3/HangmanService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    internal class HangmanService
+    {
+        string[] vocabulary = { "Softsquaregroup", "Tarveller" };
+        public void Restart()
+        {
+
+        }
+        public void GetDisplay()
+        {
+
+        }
+        public void Input()
+        {
+
+        }
+        public int GetRemainingTry()
+        {
+            return 0;
+        }
+    }
+}
